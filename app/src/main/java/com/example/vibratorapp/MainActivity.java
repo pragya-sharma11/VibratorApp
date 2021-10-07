5a9025fa5adb305b0eff6bb631fb8d13acb838c2
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-07 22:08:02 +0530

    make functionality for vibrations

--- app/src/main/java/com/example/vibratorapp/MainActivity.java
+++ app/src/main/java/com/example/vibratorapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.vibratorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT>26){
+                    ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(2000);
+                }
+                else{
+                    ((Vibrator) getSystemService(VIBRATOR_SERVICE)).vibrate(2000);
+                }
+            }
+        });
     }
 }
